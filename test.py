d35809c03b294128fc5fde17f85918c696788245
Surag <surag.noreply@gmail.com>
2021-08-02 00:30:53 +0530

    Files update

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import os
 import requests
+
 def notify(title, subtitle, message):
     #send notification
     t = '-title {!r}'.format(title)
@@ -8,13 +9,7 @@ def notify(title, subtitle, message):
     os.system('terminal-notifier {}'.format(' '.join([m, t, s])))
 
 
-
-
-
 url="https://api.quotable.io/random"
 r = requests.get(url=url)
 data = r.json()
-# print(data['author'])
-# print(data['content'])
-
-notify("Peverishhh",0,data['content']+data['author'])
+notify("More Power to you!!",data['author'],data['content'])
